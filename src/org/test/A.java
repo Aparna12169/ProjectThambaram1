c7f0a2ba5ee489248a9ce34ca111879111355810
aparna <aparnamurali038@gmail.com>
2021-05-07 18:42:45 +0530

    aaded to B

--- src/org/test/A.java
+++ src/org/test/A.java
@@ -3,7 +3,7 @@ package org.test;
 public class A {
 	public static void main(String[] args) {
 		String name = "Greens";
-String name1 ="aparna";
+		String name1 ="aparna";
 System.out.println(name1);
 int a = 20;
 System.out.println(a);
