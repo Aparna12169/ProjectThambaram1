1b9c372ba1720e5af964779d9314c5956cd0559e
aparna <aparnamurali038@gmail.com>
2021-05-07 18:34:15 +0530

    added to B

--- src/org/test/A.java
+++ src/org/test/A.java
@@ -2,17 +2,15 @@ package org.test;
 
 public class A {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		String name = "Greens";
+String name1 ="aparna";
+System.out.println(name1);
+int a = 20;
+System.out.println(a);
 		float f =1.23f;
 		System.out.println(f);
 	}
-=======
-	String name ="aparna";
-	System.out.println(name);
-	int a = 20;
-	System.out.println(a);
->>>>>>> 86798ec7edee15f53d604c6f3a3fb9cbfd79b164
+
 	
 
 }
